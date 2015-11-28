d75e1f36139a683c628e9ab8f960d2d7c407e9bc
Justin Smith <justinaverysmith@gmail.com>
2015-11-28 12:10:42 -0600

    Update and rename extreme_function.R to extreme_functions.R

--- extreme_functions.R
+++ extreme_functions.R
@@ -3,8 +3,11 @@ extremes_avg <- function (x) {
     mean <- (min(x) + max(x)) / 2
     return(mean)
 }
-
-
+# Create a function that returns min and max of a vector: extremes
+extremes <- function(x) {
+    min_max <- c(min(x), max(x))
+    return(min_max)
+}
 # Apply extremes_avg() over temp using sapply()
 sapply(temp, extremes_avg)
 
